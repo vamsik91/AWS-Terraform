e709ea795be9a944227c28587dc9919c1630febb
vamsik91 <vamsikrishnak83@gmail.com>
2023-04-07 20:02:00 -0700

    Public IP enabled

--- VPC.tf
+++ VPC.tf
@@ -6,8 +6,9 @@ resource "aws_vpc" "Example" {
   }
 }
 resource "aws_subnet" "Example_subnet" {
-  vpc_id     = aws_vpc.Example.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id                  = aws_vpc.Example.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = "true"
 
   tags = {
     Name = "Example_subnet"
