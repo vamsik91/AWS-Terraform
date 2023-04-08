347c040c7de851f97794248328a67ddcf53891a2
vamsik91 <vamsikrishnak83@gmail.com>
2023-04-07 19:59:21 -0700

    Subent added

--- VPC.tf
+++ VPC.tf
@@ -1,7 +1,15 @@
 resource "aws_vpc" "Example" {
-  cidr_block       = "10.0.0.0/16"
-  
+  cidr_block = "10.0.0.0/16"
+
   tags = {
     Name = "Example"
+  }
+}
+resource "aws_subnet" "Example_subnet" {
+  vpc_id     = aws_vpc.Example.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Example_subnet"
   }
 }
