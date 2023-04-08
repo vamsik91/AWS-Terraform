720cbee21be19fb3a4213050317f7566b5ccb3ef
vamsik91 <vamsikrishnak83@gmail.com>
2023-04-08 00:36:58 -0700

    Update NACL

--- Firewalls.tf
+++ Firewalls.tf
@@ -8,7 +8,7 @@ resource "aws_network_acl" "Example-nacl" {
 }
 
 
-#NACL Outnbound Rules
+#NACL Inbound Rules
 resource "aws_network_acl_rule" "Example-nacl-Inbound" {
   network_acl_id = aws_network_acl.Example-nacl.id
   rule_number    = 100
@@ -20,29 +20,18 @@ resource "aws_network_acl_rule" "Example-nacl-Inbound" {
   to_port        = 65335
 }
 
-#NACL Inbond-SSH-Rule
-resource "aws_network_acl_rule" "Example-nacl-SSH" {
+#NACL Outbound Rules
+resource "aws_network_acl_rule" "Example-nacl-outbound" {
   network_acl_id = aws_network_acl.Example-nacl.id
   rule_number    = 100
   egress         = true
   protocol       = "tcp"
   rule_action    = "allow"
   cidr_block     = "0.0.0.0/0"
-  from_port      = 22
-  to_port        = 22
+  from_port      = 0
+  to_port        = 65335
 }
 
-#NACL Inbound-HTTP-Rule
-resource "aws_network_acl_rule" "Example-nacl-Http" {
-  network_acl_id = aws_network_acl.Example-nacl.id
-  rule_number    = 200
-  egress         = true
-  protocol       = "tcp"
-  rule_action    = "allow"
-  cidr_block     = "0.0.0.0/0"
-  from_port      = 80
-  to_port        = 80
-}
 
 #NACL Subnet Association
 resource "aws_network_acl_association" "Example-acl-asso" {
