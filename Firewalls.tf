3e375788caf43d682496c56857fd231d0da2cad5
vamsik91 <vamsikrishnak83@gmail.com>
2023-04-07 22:48:25 -0700

    NACL outbound rules

--- Firewalls.tf
+++ Firewalls.tf
@@ -15,3 +15,25 @@ resource "aws_network_acl_rule" "Example-nacl-Inbound" {
   from_port      = 0
   to_port        = 65335
 }
+
+resource "aws_network_acl_rule" "Example-nacl-SSH" {
+  network_acl_id = aws_network_acl.Example-nacl.id
+  rule_number    = 100
+  egress         = true
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 22
+  to_port        = 22
+}
+
+resource "aws_network_acl_rule" "Example-nacl-Http" {
+  network_acl_id = aws_network_acl.Example-nacl.id
+  rule_number    = 200
+  egress         = true
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 80
+  to_port        = 80
+}
