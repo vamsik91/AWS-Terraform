974ebabdfab2444d523381d2d81964c03a79d530
vamsik91 <vamsikrishnak83@gmail.com>
2023-04-08 00:03:35 -0700

    EC2 instance

---
+++ VM.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "Example-server" {
+  ami                    = "ami-0abc4c35ba4c005ca"
+  instance_type          = "t2.micro"
+  key_name               = "key"
+  subnet_id              = aws_subnet.Example_subnet.id
+  vpc_security_group_ids = [aws_security_group.Example-sg.id]
+  userdata=file("web.sh")
+  tags = {
+    Name = "Example-Server"
+  }
+
+}
